33c2e4e1776f41f6af97de1f18656fb07906cbb9
moohamedmagdi <moohamedmagdi@gmail.com>
2023-06-08 13:05:51 -0700

    add code to hidden discovery file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+    import sys
+    index = 0
+    listed = (dir(hidden_4))
+    listlen = len(listed)
+    for elements in listed:
+        if elements[0] == '_':
+            continue
+        else:
+            print(elements)
