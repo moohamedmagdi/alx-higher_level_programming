d525f68c0c2b65666338754f59a3836848d46a6e
moohamedmagdi <moohamedmagdi@gmail.com>
2023-06-09 20:10:50 -0700

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-    if l == 0:
-        c = None
-    else:
-        c = sentence[0]
-    tup = (l, c)
-    return tup
+    total = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new = total, char
+    return(new)
