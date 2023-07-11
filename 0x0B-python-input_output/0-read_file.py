8d02bbffb20fda34b434868bb2a91396821fc6f8
moohamedmagdi <moohamedmagdi@gmail.com>
2023-07-11 10:08:13 +0000

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-'''
-Module to read and print contents of a file
-'''
 
 
 def read_file(filename=""):
-    ''' Reads file and prints contents '''
-    with open(filename) as open_file:
-        contents = open_file.read()
-    print(contents, end="")
-
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
