1bde007dc76d5256973c304bd75cbc1032549688
moohamedmagdi <moohamedmagdi@gmail.com>
2023-08-16 04:57:47 -0700

    add more edits 0,1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,9 @@
--- Creates user
--- create user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- grant privs
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- flush
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
